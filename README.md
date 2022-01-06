c4341f51fe063b4db1234dae6a07deb68e6ebb86
BaiJiFeiLong <baijifeilong@gmail.com>
2022-01-06 16:47:50 +0800

    Add build.py and README.md

---
+++ README.md
@@ -0,0 +1,38 @@
+# IceSpringPySideStubs
+
+PySide stubs with Qt signals and Qt documentations and more.
+
+Supported Qt versions now: PySide2 only.
+
+## Official sites:
+
+- Github: [https://github.com/baijifeilong/IceSpringPySideStubs](https://github.com/baijifeilong/IceSpringPySideStubs)
+- Pypi(PySide2): [https://pypi.org/project/IceSpringPySideStubs-PySide2](https://pypi.org/project/IceSpringPySideStubs-PySide2)
+
+## Features
+
+- Qt official documentations support
+- Qt signals support
+- More correct type annotations
+- Recognition of unknown method names
+- Better performance: one class one file
+- More editable: one class one file
+- Clean: all codes are well formatted
+
+## Known limitations
+
+- PySide6 support is not ready
+- PyQt5 support is not ready
+- PyQt6 support is not ready
+
+## Install
+
+- PySide2: `pip install IceSpringPySideStubs-PySide2`
+
+## Usage
+
+Out of the box
+
+## Licence
+
+GPL-3
---
+++ build.py
@@ -0,0 +1,30 @@
+# Created by BaiJiFeiLong@gmail.com at 2022/1/6 12:46
+
+import distutils.log
+import os
+
+import setuptools.dist
+from pathlib3x import Path
+
+os.chdir(Path(__file__).parent / "target/PySide2Stubs")
+for path in Path(".").glob("*"):
+    if not path.stem.endswith("-stubs"):
+        path.rmtree()
+
+readme = Path("../../README.md").read_text(encoding="utf8")
+distutils.log.set_verbosity(0)
+for command in ["sdist", "bdist_wheel"]:
+    setuptools.dist.Distribution(attrs=dict(
+        script_name="",
+        name='IceSpringPySideStubs-PySide2',
+        url="https://github.com/baijifeilong/IceSpringPySideStubs",
+        license='GPL3',
+        author='BaiJiFeiLong',
+        author_email='baijifeilong@gmail.com',
+        version='1.0.0',
+        description='PySide2 stubs with Qt signals and Qt documentations and more',
+        packages=["PySide2-stubs"],
+        package_data={"PySide2-stubs": ['*.pyi', '**/*.pyi']},
+        long_description=readme,
+        long_description_content_type='text/markdown'
+    )).run_command(command)
